86bf2f50cf52a61f4c214976ebd7e06fabe85b7c
galahadtrail <zzzzrzzzz@yandex.ru>
2024-11-17 00:22:30 +0300

    now place matters

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def  is_letter_in_word(black, word):
             return True
     return False
 
+def is_letter_in_forbidden(letter, dct):
+    for let in dct:
+        if letter == let:
+            return True
+    return False
+
 russian_letters = ["а", "б", "в", "г", "д", "е", "ж", "з", "и", "к", 
 "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ы", "ъ", "э", "ь", "ю", "я"]
 
@@ -14,7 +20,7 @@ words = []
 with open("russian.utf-8", "r+") as file:
     temp = file.read()
 
-words = temp.split("\n")
+words = set(temp.split("\n"))
 result = []
 
 for word in words:
@@ -52,48 +58,35 @@ for word in finally_black_result:
     if is_letter_in_word(white_list, word):
         finally_result.append(word)
 
-print(sorted(finally_result))
-
 finally_result = set(finally_result)
 
 print("Please type which letters can not be at some places. Type q to interrupt the string\n")
 
 first_letter = []
-while True:
-    letter = str(input())
-    if letter != "q":
-        first_letter.append(letter)
-    else:
-        break
+letter = str(input())
+first_letter = letter.split(",")
     
-second_letter = []
-while True:
-    letter = str(input())
-    if letter != "q":
-        second_letter.append(letter)
-    else:
-        break
+first_letter = []
+letter = str(input())
+second_letter = letter.split(",")
 
-third_letter = []
-while True:
-    letter = str(input())
-    if letter != "q":
-        third_letter.append(letter)
-    else:
-        break
+first_letter = []
+letter = str(input())
+third_letter = letter.split(",")
 
-forth_letter = []
-while True:
-    letter = str(input())
-    if letter != "q":
-        forth_letter.append(letter)
-    else:
-        break
+first_letter = []
+letter = str(input())
+forth_letter = letter.split(",")
 
-fifth_letter = []
-while True:
-    letter = str(input())
-    if letter != "q":
-        fifth_letter.append(letter)
-    else:
-        break
+first_letter = []
+letter = str(input())
+fifth_letter = letter.split(",")
+
+
+res = []
+
+for word in finally_result:
+    if not is_letter_in_forbidden(word[0], first_letter) and not is_letter_in_forbidden(word[1], second_letter) and not is_letter_in_forbidden(word[2], third_letter) and not is_letter_in_forbidden(word[3], forth_letter) and not is_letter_in_forbidden(word[4], fifth_letter):
+        res.append(word)
+print(res)
+print(len(res))
